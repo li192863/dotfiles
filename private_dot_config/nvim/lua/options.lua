f4c876204faee89991a7d0116cc28a188dde18e5
Chestnut <li1928631@163.com>
2024-05-13 20:50:33 +0800

    修改nvim相关配置，增加undofile相关配置
    退出nvim后，再次进入nvim，使用undo将无效
    在此之前，重新进入nvim时使用undo仍将继续撤销

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -3,5 +3,6 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
-o.relativenumber = true
+o.cursorlineopt = "both" -- enable cursorline!
+o.relativenumber = true -- enable relative number
+o.undofile = false -- disable undo file after quiting the editer

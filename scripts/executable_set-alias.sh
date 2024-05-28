788fe0d4fecd6d9492bc350ed98fd4cc23e80bdf
Chestnut <li1928631@163.com>
2024-05-28 21:53:23 +0800

    修改别名，新增ce/ci/ni/nig/pi等别名
    conda/npm/pip的常见操作操作将更加方便

--- scripts/executable_set-alias.sh
+++ scripts/executable_set-alias.sh
@@ -14,12 +14,19 @@ alias c='cat'
 
 # conda
 alias ca='conda activate'
+alias ce='conda env list'
+alias ci='conda install'
 
 # echo
 alias e='echo'
 
+# node
+alias ni='npm install'
+alias nig='npm install -g'
+
 # python
 alias p='ipython'
+alias pi='pip install'
 
 # tail
 alias tf='tail -f'

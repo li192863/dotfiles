4917cfa24b56d6a2fafc91e9ae4c5b20f4edc787
Chestnut <li1928631@163.com>
2024-09-25 01:16:04 +0800

    修改别名，提高v/p可用性，修改c为clear

--- scripts/executable_set-alias.sh
+++ scripts/executable_set-alias.sh
@@ -9,8 +9,8 @@
 alias au='sudo apt-get update'
 alias ai='sudo apt-get update && sudo apt-get install'
 
-# cat
-alias c='cat'
+# clear
+alias c='clear'
 
 # conda
 alias ca='conda activate'
@@ -25,14 +25,18 @@ alias e='echo'
 alias nl='npm list'
 alias ni='npm install'
 
-# ipython
-alias p='python'
+# python, ipython
+if command -v ipython >/dev/null 2>&1; then
+  alias p='ipython'
+elif command -v python3 >/dev/null 2>&1; then
+  alias p='python3'
+fi
 
 # python
-alias pa='python -m venv venv && . venv/bin/activate'
+alias pa='python3 -m venv venv && . venv/bin/activate'
 alias pe='deactivate' # python-venv exit
-alias pl='pip list'
-alias pi='pip install'
+alias pl='pip3 list'
+alias pi='pip3 install'
 
 # tar
 alias tc='tar czf'
@@ -41,8 +45,14 @@ alias tx='tar xf'
 # tail
 alias t='tail'
 
-# nvim
-alias v='nvim'
+# nvim, vim, vi
+if command -v nvim >/dev/null 2>&1; then
+  alias v='nvim'
+elif command -v vim >/dev/null 2>&1; then
+  alias v='vim'
+else
+  alias v='vi'
+fi
 
 # workspace
 alias ws='tmux new-session -A -s main'
